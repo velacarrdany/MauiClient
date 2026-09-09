091fde0b63192c36983340371ffcbea8f961a973
velacandledev <velacandledev@gmail.com>
2026-09-08 19:08:30 -0600

    Corrections and added "Car" class

---
+++ MauiClient/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiClient
+{
+    public class Car
+    {
+        public int Id {  get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public double Price { get; set; }
+        public DateTime Release { get; set; }
+    }
+}

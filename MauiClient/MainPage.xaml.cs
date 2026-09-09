14550405b5f9afb9a18e1b98ae79c8e9a817e2f9
velacandledev <velacandledev@gmail.com>
2026-09-08 19:13:11 -0600

    Filled class btnSave_Clicked

--- MauiClient/MainPage.xaml.cs
+++ MauiClient/MainPage.xaml.cs
@@ -9,7 +9,11 @@
 
         private void btnSave_Clicked(object sender, EventArgs e)
         {
-
+            Car car = new Car();
+            car.Brand = txtBrand.Text;
+            car.Model = txtModel.Text;
+            car.Price = Convert.ToDouble(txtPrice.Text);
+            car.Release = txtRelease.Date.Value;
         }
     }
 }

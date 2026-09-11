20889cc95021288bbc23dd8c8faa0e37747a826f
velacandledev <velacandledev@gmail.com>
2026-09-10 19:49:07 -0600

    A bunch of stuff idk

--- MauiClient/Index.xaml.cs
+++ MauiClient/Index.xaml.cs
@@ -1,13 +1,23 @@
+using RestSharp;
+using Newtonsoft.Json;
+
 namespace MauiClient;
 
 public partial class Index : ContentPage
 {
+	public const string url = "url";
 	public Index()
 	{
 		InitializeComponent();
+        Loaded += Index_Loaded;
 	}
 
-	private void btnAdd_Clicked(object sender, EventArgs e)
+    private void Index_Loaded(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void btnAdd_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new MainPage());
     }

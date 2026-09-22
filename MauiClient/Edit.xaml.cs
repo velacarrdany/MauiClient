0da5d3093862ae4e496997a28e3204553ef46146
velacandledev <velacandledev@gmail.com>
2026-09-21 18:31:03 -0600

    Filled more stuff on Edit.xaml.cs

--- MauiClient/Edit.xaml.cs
+++ MauiClient/Edit.xaml.cs
@@ -2,8 +2,22 @@ namespace MauiClient;
 
 public partial class Edit : ContentPage
 {
+
+    Car car;
+
 	public Edit()
 	{
 		InitializeComponent();
 	}
+
+    public Edit(Car _car)
+    {
+        InitializeComponent();
+        car = _car;
+    }
+
+    private void btnSave_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
